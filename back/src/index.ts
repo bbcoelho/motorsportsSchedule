21e5a99f37d413986956cd64b8bb8a3f799fc1a2
Bruno Coelho <bbcoelho@gmail.com>
2024-08-08 17:59:51 -0300

    feat: serve file data.json

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,15 @@
+import { readFileSync } from 'fs';
+
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        return new Response("Bun!");
+        const jsonFilePath = "../data/data.json";
+        // Read the JSON file
+        const jsonData = readFileSync(jsonFilePath, "utf-8");
+        // Respond with the JSON data
+        return new Response(jsonData, {
+            headers: { "Content-Type": "application/json" },
+        });
     },
 });
 
