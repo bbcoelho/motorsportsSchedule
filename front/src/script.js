e36ff5e24c49bf1607b9068679eaa6616ef1ca36
Bruno Coelho <bbcoelho@gmail.com>
2024-08-22 18:09:10 -0300

    chore: add typescript

--- front/src/script.js
+++ front/src/script.js
@@ -1,24 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function () {
     // URL of the JSON file
     const url = 'http://127.0.0.1:3000';
-
     // Fetch the JSON file
-    fetch(url)
+    let content = "";
+    await fetch(url)
         .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json(); // Parse the JSON response
-        })
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json(); // Parse the JSON response
+    })
         .then(data => {
-            // Convert JSON data to a string with pretty print
-            const jsonString = JSON.stringify(data, null, 2);
-
-            // Set the JSON string as the content of the HTML element
-            document.getElementById('motoGPDate').textContent = jsonString;
-        })
+        // Convert JSON data to a string with pretty print
+        const jsonString = JSON.stringify(data, null, 2);
+        // Set the JSON string as the content of the HTML element
+        content = jsonString;
+    })
         .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-            document.getElementById('motoGPDate').textContent = 'Error loading JSON data.';
-        });
+        console.error('There was a problem with the fetch operation:', error);
+        content = 'Error loading JSON data.';
+    });
+    const element = document.getElementById('motoGPDate');
+    if (element) {
+        element.textContent = content;
+    }
 });
+export {};
---
+++ front/src/scripts.ts
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    // URL of the JSON file
+    const url = 'http://127.0.0.1:3000';
+
+    // Fetch the JSON file
+    let content: string = "";
+
+    await fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json(); // Parse the JSON response
+        })
+        .then(data => {
+            // Convert JSON data to a string with pretty print
+            const jsonString = JSON.stringify(data, null, 2);
+            // Set the JSON string as the content of the HTML element
+            content = jsonString;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+            content = 'Error loading JSON data.';
+        });
+
+    const element = document.getElementById('motoGPDate');
+    if (element) {
+        element.textContent = content;
+    }
+    
+});
