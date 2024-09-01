47647e1d9a47125675f2261d0f87b0f23e67e663
Bruno Coelho <bbcoelho@gmail.com>
2024-09-01 15:10:50 -0300

    feat: add cache and fetch

--- front/src/sw.ts
+++ front/src/sw.ts
@@ -5,8 +5,39 @@ import { NavigationRoute, registerRoute } from 'workbox-routing'
 declare let self: ServiceWorkerGlobalScope
 
 self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING')
-    self.skipWaiting()
+	if (event.data && event.data.type === 'SKIP_WAITING')
+		self.skipWaiting()
+})
+
+// cache backend response on service worker install
+const urlsToCache = ["https://motorsportsschedule.onrender.com/"];
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches.open('pwa-urls')
+			.then(cache => {
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request).then(cachedResponse => {
+			const networkFetch = fetch(event.request).then(response => {
+				// update the cache with a clone of the network response
+				const responseClone = response.clone()
+				caches.open('pwa-urls').then(cache => {
+					cache.put(event.request, responseClone)
+				})
+				return response
+			}).catch(function (reason) {
+				console.error('ServiceWorker fetch failed: ', reason)
+			})
+			// prioritize cached response over network
+			return cachedResponse || networkFetch
+		}
+		)
+	)
 })
 
 // self.__WB_MANIFEST is the default injection point
@@ -19,10 +50,10 @@ cleanupOutdatedCaches()
 let allowlist
 // in dev mode, we disable precaching to avoid caching issues
 if (import.meta.env.DEV)
-  allowlist = [/^\/$/]
+	allowlist = [/^\/$/]
 
 // to allow work offline
 registerRoute(new NavigationRoute(
-  createHandlerBoundToURL('index.html'),
-  { allowlist },
+	createHandlerBoundToURL('index.html'),
+	{ allowlist },
 ))
