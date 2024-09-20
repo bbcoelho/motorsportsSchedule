f4fc7162c01f6a89ad505e6cac72d354c54607e3
Bruno Coelho <bbcoelho@gmail.com>
2024-09-20 10:04:03 -0300

    chore: change theme color

--- front/vite.config.js
+++ front/vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
       name: 'Motorsports Schedule',
       short_name: '⏰',
       description: 'Motorsports events schedule all in one place',
-      theme_color: '#3367D6',
+      theme_color: '#ff0000',
       icons: [
         {
           "src": "pwa-64x64.png",
